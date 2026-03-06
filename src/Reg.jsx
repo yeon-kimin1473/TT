687e256c28dcee49f82424a484d877998315fd14
yeon-kimin1473 <149294303+yeon-kimin1473@users.noreply.github.com>
2026-03-06 11:01:23 +0530

    Update registration API endpoint URL

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -19,7 +19,7 @@ function Reg()
       {
         //  const res=await axios.post("http://localhost:8080/reg",data)
         const res = await axios.post(
-        "https://cabsystemsms-1.onrender.com/register",
+        "https://tt-6t31.onrender.com",
         data
       )
          alert(res.data)
