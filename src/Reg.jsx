ef0421470a894d136383713fda865e6228d3b30a
yeon-kimin1473 <149294303+yeon-kimin1473@users.noreply.github.com>
2026-03-06 11:12:12 +0530

    Update Reg.jsx

--- src/Reg.jsx
+++ src/Reg.jsx
@@ -19,7 +19,7 @@ function Reg()
       {
         //  const res=await axios.post("http://localhost:8080/reg",data)
         const res = await axios.post(
-        "https://tt-6t31.onrender.com",
+        "https://tt-6t31.onrender.com/register",
         data
       )
          alert(res.data)
